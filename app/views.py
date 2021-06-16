4f6f4df56a5beb164212587c1ba5bdf602755acf
Luciano <lucholeyria@gmail.com>
2021-06-16 00:12:19 -0300

    Configuracion timezone

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import now
 
 # Create your views here.
 def index(request):
   context = {}
+  print(now())
   return render(request, 'app/list.html', context)
 
 def new(request):
