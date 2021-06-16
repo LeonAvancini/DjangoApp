2adafba1199ee64a84ab13b904e18d0d3588264b
Luciano <lucholeyria@gmail.com>
2021-06-15 23:39:33 -0300

    Se vincularon los archivos staticos y se creo el formulario

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+  context = {}
+  return render(request, 'app/list.html', context)
+
+def new(request):
+  context = {}
+  return render(request, 'app/new.html', context)
