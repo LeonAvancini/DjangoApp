0ee639897b4914107b3a3c9377b7e3b362ef7deb
Luciano <lucholeyria@gmail.com>
2021-06-22 20:29:23 -0300

    Correccion de errores

--- app/views.py
+++ app/views.py
@@ -23,7 +23,6 @@ def ArticulosList(request):
         articuloUno = Articulo()
         articuloUno.titulo = request.POST["titulo"]
         articuloUno.descripcion = request.POST["descripcion"]
-        print(request.POST['imagen'])
         articuloUno.save()
 
     articulos = Articulo.objects.all()
